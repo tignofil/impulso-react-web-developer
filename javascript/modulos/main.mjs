f6ad5e60cb73696033a8eb3bad347c3fd8e0481d
tignofil <tignofil@gmail.com>
2021-11-09 01:03:19 -0300

    add exercicio Modulos/ECMA/OOJS  no diretorio javascript

---
+++ javascript/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Fabio", 42));
+console.log(mostraCidade("Fabio", "Rio de Janeiro"));
+console.log(mostraHobby("Fabio", "Capoeira"));
